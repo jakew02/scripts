fbd1415fec0cc801892158a3f8f8c0e3c0c71964
Jake Whatley <jacob.whatley91@gmail.com>
2016-10-08 15:33:56 -0400

    changes for update-default-branch.sh

--- update-default-branch.sh
+++ update-default-branch.sh
@@ -2,12 +2,12 @@
 #
 # small script to update the default branch for all projects
 
-if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
+if [ -z $1 ] || [ -z $2 ]; then
   echo ""
   echo "************************************************************************************"
   echo "*                                                                                  *"
-  echo "* Usage: ./update-default-branch.sh <project list> <github username> <branch name> *"
-  echo "* Example: ./update-default-branch.sh jakew02 android-6.0                          *"
+  echo "* Usage: ./update-default-branch.sh <project list> <branch name>                   *"
+  echo "* Example: ./update-default-branch.sh project-list android-7.0                     *"
   echo "*                                                                                  *"
   echo "************************************************************************************"
   echo ""
@@ -26,11 +26,11 @@ while read file;
     echo ""
     echo "====================================================================="
     echo " PROJECT NAME: {$project}"
-    echo " BRANCH NAME: {$3}"
+    echo " BRANCH NAME: {$2}"
     echo "====================================================================="
     echo ""
 
-    curl -u $2 -d "{\"name\":\"$project\", \"default_branch\":\"$3\"}" https://api.github.com/repos/jakew02/$project
+    curl -u jakew02 -d "{\"name\":\"$project\", \"default_branch\":\"$2\"}" https://api.github.com/repos/jakew02/$project
 
 done < project-list
 
